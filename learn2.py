5d16f8c83a428def03b62c157a432d4b2a07080c
Ayushranjan5510 <ayushranjan5510@gmail.com>
2025-07-22 17:57:57 +0530

    Added Subtraction

---
+++ learn2.py
@@ -0,0 +1,4 @@
+print("Welcome to python\nLets subtract two integers!!\n")
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+print("Difference between the two numbers is ",(a-b))
